83f5de8809fbff8d745ce9906876c96157e4b9f1
Kevin Pravia <keserrano00@gmail.com>
2022-02-03 16:07:01 -0700

    restore session user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,4 +29,18 @@ router.delete('/', (_req, res) => {
     return res.json({ message: 'success' });
 });
 
+// restore session user
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+
+    if(user) {
+        return res.json({
+            user: user.toSafeObject()
+        });
+    }
+    else {
+        return res.json({});
+    }
+});
+
 module.exports = router;
