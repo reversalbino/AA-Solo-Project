8cc7dba4f83daf2a52d4f533e378f20361ad2b05
Kevin Pravia <keserrano00@gmail.com>
2022-02-03 15:44:36 -0700

    finish logout

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,31 +1,46 @@
+// const router = require('express').Router();
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// const { restoreUser } = require('../../utils/auth.js');
+// const { requireAuth } = require('../../utils/auth.js');
+
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get('/restore-user', restoreUser,(req, res) => {
+//   return res.json(req.user);
+// });
+
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// router.post('/test', function(req, res) {
+//   res.json({requestBody: req.body });
+// });
+
+// module.exports = router;
+
+// backend/routes/api/index.js
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.get('/require-auth', requireAuth, (req, res) => {
-    return res.json(req.user);
-  }
-);
+router.use('/session', sessionRouter);
 
-router.get('/restore-user', restoreUser,(req, res) => {
-  return res.json(req.user);
-});
+router.use('/users', usersRouter);
 
-router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
-
-router.post('/test', function(req, res) {
-  res.json({requestBody: req.body });
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
 });
 
 module.exports = router;
