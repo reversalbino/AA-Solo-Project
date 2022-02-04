7cd2bc973835dc8fc1a5af5289a275c71b97f4a5
Kevin Pravia <keserrano00@gmail.com>
2022-02-03 17:13:13 -0700

    finish validating login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,11 +1,27 @@
 // backend routes/api/session.js
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
-router.post('/', asyncHandler(async (req, res, next) => {
+// Make sure a credential and password were provided
+const validateLogin = [
+    check('credential').exists({ checkFalsy: true }).notEmpty().withMessage(
+        'Please provide a valid email or username.'
+    ),
+    check('password').exists({ checkFalsy: true }).notEmpty().withMessage(
+        'Please provide a password.'
+    ),
+    handleValidationErrors
+];
+
+//Log in
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
     const user = await User.login({ credential, password });
 
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -6,11 +6,12 @@ const { validationResult } = require('express-validator');
 const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
 
-  if(!validationErrors) {
+  if(!validationErrors.isEmpty()) {
     const errors = validationErrors.array().map(error => `${error.msg}`);
 
     const err = Error('Bad request.');
-    err.errors = 400;
+    err.errors = errors;
+    err.status = 400;
     err.title = 'Bad request.';
     next(err);
   }
