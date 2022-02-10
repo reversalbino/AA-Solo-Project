7de8d811ecb9d6359e04705f5f97c2db255ccd1c
Kevin Pravia <keserrano00@gmail.com>
2022-02-09 23:47:50 -0700

    get song (almost)

--- frontend/src/components/SongInfoPage/index.js
+++ frontend/src/components/SongInfoPage/index.js
@@ -1,24 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 
 import * as sessionActions from '../../store/session';
 
 function SongInfoPage() {
     const dispatch = useDispatch();
-    const [song, setSong] = useState(undefined);
+    // const [song, setSong] = useState(undefined);
     
-    async function test() {
-        const id = 1;
-        await setSong(dispatch(sessionActions.getSong(id)));
-        setSong(window.localStorage.getItem('data'))
-        console.log('IN SONG DETAIL PAGE', song);
-    }
+    // async function test() {
+    //     const id = 1;
+    //     await setSong(dispatch(sessionActions.getSong(id)()));
+    //     setSong(window.localStorage.getItem('data'))
+    //     console.log('IN SONG DETAIL PAGE', song);
+    // }
     
-    useEffect(() => {
-       
-        test();
-        
-    }, [])
+    // useEffect(() => {
+    //     test();  
+    // }, []);
+    let song;
+
+    dispatch(sessionActions.getSong(1)).then(result => {
+        console.log('RESULT: ', result.file.data);
+        song = result.file.data;
+    });
+    //const song = getSongFunction();
+    
+    // console.log(typeof getSongFunction, getSongFunction)
+    
+    // console.log(Object.keys(getSongFunction));
+
+    console.log('SONG', song);
 
     return (
         <>
@@ -26,7 +37,7 @@ function SongInfoPage() {
                 <source src={song} type='/audio/mp3' />
             </audio>
         </>
-    )
+    );
 }
 
 export default SongInfoPage;
